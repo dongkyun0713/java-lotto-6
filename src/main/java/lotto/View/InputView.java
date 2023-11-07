847fa0b25226dc86b767b378f77e90572897b5d5
dongkyun <kyun010713@gmail.com>
2023-11-08 00:59:13 +0900

    feat(view): 입력받는 클래스 생성 및 당첨 번호를 입력받는 기능 추가

--- src/main/java/lotto/View/InputView.java
+++ src/main/java/lotto/View/InputView.java
@@ -5,5 +5,8 @@ import camp.nextstep.edu.missionutils.Console;
 public class InputView {
     private static final String INPUT_MONEY_MESSAGE = "구입금액을 입력해 주세요.";
 
-   
+    public String inputMoney() {
+        System.out.println(INPUT_MONEY_MESSAGE);
+        return Console.readLine();
+    }
 }
