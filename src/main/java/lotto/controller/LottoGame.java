d7b2cbe4ad5e0d628a85ddf80f803cf9a5310fb5
dongkyun <kyun010713@gmail.com>
2023-11-07 23:20:54 +0900

    feat(controller): 구입금액 입력받는 기능 추가

--- src/main/java/lotto/controller/LottoGame.java
+++ src/main/java/lotto/controller/LottoGame.java
@@ -1,7 +1,10 @@
 package lotto.controller;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class LottoGame {
     public void start() {
         System.out.println("구입금액을 입력해 주세요.");
+        int purchaseAmount = Integer.parseInt(Console.readLine());
     }
 }
