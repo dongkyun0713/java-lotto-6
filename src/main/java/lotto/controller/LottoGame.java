454d834d2204efca2f8265a0315525f934710124
dongkyun <kyun010713@gmail.com>
2023-11-08 01:23:13 +0900

    feat(View): inputMoney 메소드 int형으로 수정

--- src/main/java/lotto/controller/LottoGame.java
+++ src/main/java/lotto/controller/LottoGame.java
@@ -2,9 +2,11 @@ package lotto.controller;
 
 import camp.nextstep.edu.missionutils.Console;
 import lotto.View.InputView;
+import lotto.View.OuputView;
 
 public class LottoGame {
     private final InputView inputView = new InputView();
+    private final OuputView outputView = new OuputView();
     public void start() {
         inputView.inputMoney();  // 구입 금액 입력
 
