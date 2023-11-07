d004fa05bfe48ec8dc6352b8065f3430a69366c4
dongkyun <kyun010713@gmail.com>
2023-11-08 01:04:13 +0900

    feat(view): 당첨번호 입력받는 기능 추가

--- src/main/java/lotto/View/InputView.java
+++ src/main/java/lotto/View/InputView.java
@@ -4,9 +4,15 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
     private static final String INPUT_MONEY_MESSAGE = "구입금액을 입력해 주세요.";
+    private static final String INPUT_WINNING_NUMBER_MESSAGE = "구입금액을 입력해 주세요.";
 
     public String inputMoney() {
         System.out.println(INPUT_MONEY_MESSAGE);
+        return Console.readLine();
+    }
+
+    public String inputWinningNumber() {
+        System.out.println(INPUT_WINNING_NUMBER_MESSAGE);
         return Console.readLine();
     }
 }
