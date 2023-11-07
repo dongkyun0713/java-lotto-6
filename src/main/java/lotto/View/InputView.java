b01858d750ec7ea69209832d9a0c841cd6a54af3
dongkyun <kyun010713@gmail.com>
2023-11-08 01:23:03 +0900

    feat(View): inputMoney 메소드 int형으로 수정

--- src/main/java/lotto/View/InputView.java
+++ src/main/java/lotto/View/InputView.java
@@ -7,9 +7,9 @@ public class InputView {
     private static final String INPUT_WINNING_NUMBER_MESSAGE = "구입금액을 입력해 주세요.";
     private static final String INPUT_BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요";
 
-    public String inputMoney() {
+    public int inputMoney() {
         System.out.println(INPUT_MONEY_MESSAGE);
-        return Console.readLine();
+        return Integer.parseInt(Console.readLine());
     }
 
     public String inputWinningNumber() {
