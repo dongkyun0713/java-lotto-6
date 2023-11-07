fd8ecd06e54e150d7191f4b8a97f59107e285cec
dongkyun <kyun010713@gmail.com>
2023-11-08 01:16:38 +0900

    feat(controller): controller에 구입금액 메소드 추가

--- src/main/java/lotto/controller/LottoGame.java
+++ src/main/java/lotto/controller/LottoGame.java
@@ -1,9 +1,12 @@
 package lotto.controller;
 
 import camp.nextstep.edu.missionutils.Console;
+import lotto.View.InputView;
 
 public class LottoGame {
+    private final InputView inputView = new InputView();
     public void start() {
+        inputView.inputMoney();  // 구입 금액 입력
 
     }
 
